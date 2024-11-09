48f2e7e929b0d0c6696f3cbd4a0fd41130f9b16e
Finomnis <finomnis@gmail.com>
2024-11-09 13:45:03 +0100

    Update text color of sample to 0xff00ff

--- samples/modules/lvgl/screen_transparency/src/main.c
+++ samples/modules/lvgl/screen_transparency/src/main.c
@@ -25,7 +25,7 @@ static void initialize_gui()
 	/*Create a white label, set its text and align it to the center*/
 	lv_obj_t *label = lv_label_create(lv_screen_active());
 	lv_label_set_text(label, "Hello world");
-	lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
+	lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xff00ff), LV_PART_MAIN);
 	lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 }
 
