47af76c70334de870e82ae81a4dfb60987946a5c
Martin Stumpf <finomnis@gmail.com>
2026-07-05 16:05:08 +0200

    More fixes in config

--- build_nrf52840dongle_mcumgr.sh
+++ build_nrf52840dongle_mcumgr.sh
@@ -2,28 +2,6 @@
 
 # . ../.venv/bin/activate
 
-# west build -b nrf52840dongle/nrf52840/bare \
-#     samples/subsys/mgmt/mcumgr/smp_svr \
-#     --sysbuild \
-#     -- \
-#     -DEXTRA_CONF_FILE="cdc.conf;fs.conf;shell.conf;shell-mgmt.conf" \
-#     -DEXTRA_DTC_OVERLAY_FILE="usb.overlay" \
-#     -DCONFIG_MCUMGR_GRP_OS_MCUMGR_PARAMS=y \
-#     -DCONFIG_MCUMGR_TRANSPORT_NETBUF_SIZE=4096 \
-#     -DCONFIG_MCUMGR_TRANSPORT_WORKQUEUE_STACK_SIZE=8192 \
-#     -DCONFIG_SHELL_BACKEND_DUMMY_BUF_SIZE=4096 \
-#     -DCONFIG_LOG=n
-
-# WORKS:
-# west build -b nrf52840dongle/nrf52840/bare \
-#     samples/subsys/mgmt/mcumgr/smp_svr \
-#     --sysbuild \
-#     -- \
-#     -DEXTRA_CONF_FILE="cdc.conf;fs.conf;shell.conf;shell-mgmt.conf" \
-#     -DEXTRA_DTC_OVERLAY_FILE="usb.overlay" \
-#     -DCONFIG_MCUMGR_GRP_OS_MCUMGR_PARAMS=y
-
-
 west build -b nrf52840dongle/nrf52840/bare \
     samples/subsys/mgmt/mcumgr/smp_svr \
     --sysbuild \
